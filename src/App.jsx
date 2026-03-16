9bafdbf88f14f0a25b3c38af5f36c2c16b27e5cc
megha-56 <meghapanthi435@gmail.com>
2026-03-17 00:25:59 +0530

    added another page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Header from './components/header'
+import Home2 from './components/home2'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <Header/>
+      <Home2/>
     </>
   )
 }
---
+++ src/components/home2.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home2=()=>{
+    return(
+        <div className=" h-200 bg-pink-200"> 
+           <div className="flex justify-center w-full ">
+             <h4 className="text-pink-700 pt-25">OUR PRODUCTS</h4>
+           </div>
+           <div className="flex justify-center w-full ">
+             <h1 className="text-pink-700 text-center font-bold text-4xl pt-5">REAL ICECREAM FOR REAL<br/> PEOPLE !</h1>
+           </div>
+
+
+          
+
+        </div>
+    )
+}
+export default Home2
