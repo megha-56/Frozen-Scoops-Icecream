780c33f40c9e9c5151e18c21198d744a74464fb4
megha-56 <meghapanthi435@gmail.com>
2026-03-19 00:47:59 +0530

    updated header-background again

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,20 +3,14 @@ import React from "react";
 
 const Header=()=>{
     return(
-         <div className="relative h-64 w-screen overflow-hidden flex items-center justify-center">
-
-            <video autoPlay loop muted playsInline 
-             className="absolute inset-0 w-full h-full object-cover object-center object-[center_-50px]  " >
-             <source src="/bg.mp4" type="video/mp4" /> 
-             </video>
-
-            <div className="absolute inset-0 bg-black/30"></div>
-              
-            <div className="relative z-10 text-white text-xl"> Your Content Here </div> 
-             
-        </div>
+        
        
-    
+    <div
+          className="h-64 w-screen bg-cover bg-[center_-25px] flex items-center justify-center gap-4"
+         style={{ backgroundImage: `url("/logo.png")` }} >
+                                                
+        
+      </div>
 
 
 
